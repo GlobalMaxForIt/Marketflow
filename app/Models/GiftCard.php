47a4f598e2114e749af2632024a9bf12ebc04215
Elyor <ernazarovelyor1004@gmail.com>
2025-01-03 17:10:08 +0500

    model to'g'rilandi

--- app/Models/GiftCard.php
+++ app/Models/GiftCard.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class giftCard extends Model
+class GiftCard extends Model
 {
     use HasFactory;
 
