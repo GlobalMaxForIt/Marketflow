bf6b070d5a7abd33d50539f18239a3a3e2d54329
Elyor <ernazarovelyor1004@gmail.com>
2025-01-03 17:33:19 +0500

    gift_card table tuzatildi

--- database/migrations/2023_10_10_044005_create_gift_card.php
+++ database/migrations/2023_10_10_044005_create_gift_card.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('percent', 100)->nullable();
-            $table->unsignedDouble('price', 15, 8)->nullable();
-            $table->unsignedDouble('min_price', 15, 8)->nullable();
+            $table->unsignedInteger('price')->nullable();
+            $table->unsignedInteger('min_price')->nullable();
             $table->timestamp('start_date')->nullable();
             $table->timestamp('end_date')->nullable();
             $table->unsignedTinyInteger('status')->default(1);
